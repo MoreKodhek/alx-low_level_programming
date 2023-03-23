f5e8b9ab5a2877707041129a68d92d8a4ddd240b
MoreKodhek <morekodhek@gmail.com>
2023-03-23 09:42:10 -0700

    modifications to header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,15 +6,16 @@
  * @c: type
  * @f: the function to send it to
  */
-typedef struct check
+/*typedef struct check
 {
 char *chk;
 void (*f)(va_list list);
-} check_t;
+} check_t;*/
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
