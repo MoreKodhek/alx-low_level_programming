726b8ba3143773058339fe2ae33be52b592bbc2e
MoreKodhek <morekodhek@gmail.com>
2023-04-04 00:27:30 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a linked list of integers
+ * @head: Pointer to the first node of the list
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *head)
+{
+	size_t count = 0;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		count++;
+		head = head->next;
+	}
+
+	return count;
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this is the readme for 0x13-more_singly_linked_lists
