85812a82adbd7bfd4d74ba9c77e25f69d2444baa
MoreKodhek <morekodhek@gmail.com>
2023-06-09 13:37:38 -0700

    attempt

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - index of a key
+ * @key: Key
+ * @size: the size of the array
+ *
+ * Return: index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
